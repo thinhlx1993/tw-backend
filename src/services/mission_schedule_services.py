92e933f8162c3b892cba20a3c749c38517c44d70
thinh <thinhle.ict@gmail.com>
2024-02-07 14:29:15 +0700

    hot fix

--- src/services/mission_schedule_services.py
+++ src/services/mission_schedule_services.py
@@ -223,7 +223,7 @@ def find_unique_interaction_partner(
             *additional_filters
         )
         .order_by(clicks_count_subquery.c.clicks_count.asc())
-        .limit(10)
+        .limit(15)
         .all()
     )
 
