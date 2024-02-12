37ee0ba3952649a3bbd9aa5508b4f2ccdd0c7d97
thinh <thinhle.ict@gmail.com>
2024-02-13 02:37:25 +0700

    ok fix get tasks

--- src/services/mission_schedule_services.py
+++ src/services/mission_schedule_services.py
@@ -402,7 +402,7 @@ def get_profile_with_event_count_below_limit_v2(event_type):
             Profiles.is_disable.is_(False),
         )
         .order_by(func.random())
-        .limit(3)
+        .limit(10)
         .all()
     )
 
