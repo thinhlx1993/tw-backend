1b73af076d8ddde264392d3dc4825ae8781e1e5b
thinh <thinhle.ict@gmail.com>
2024-02-19 18:26:19 +0700

    update revision number

--- migrations/versions/7673e33cddb8_extend_fa_length.py
+++ migrations/versions/7673e33cddb8_extend_fa_length.py
@@ -10,17 +10,17 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '7673e33cddb8'
-down_revision = '40bf9ca78a0d'
+revision = "7673e33cddb8"
+down_revision = "8c8d680aba0a"
 branch_labels = None
 depends_on = None
 
 
 def upgrade():
     # Use op.alter_column to modify the column
-    op.alter_column('profiles', 'fa', type_=sa.Text())
+    op.alter_column("profiles", "fa", type_=sa.Text())
 
 
 def downgrade():
     # In case of downgrade, revert the changes
-    op.alter_column('profiles', 'fa', type_=sa.String(length=128))
+    op.alter_column("profiles", "fa", type_=sa.String(length=128))
