b2f61636c57c5cc134a2bef744116c818ef8a339
thinh <thinhle.ict@gmail.com>
2024-02-23 13:51:41 +0700

    update click count

--- src/tasks/schedule.py
+++ src/tasks/schedule.py
@@ -6,57 +6,59 @@ def update_click(teams_id):
     print("update_click_count", teams_id)
     with db.app.app_context():
         db.session.execute("SET search_path TO public, 'cs_" + str(teams_id) + "'")
-        sql_query = (
-            f"\n"
-            f"    WITH Clicks AS (\n"
-            f"        SELECT\n"
-            f"            g.group_id,\n"
-            f"            COUNT(e.profile_id_interact) AS total_clicks_giver\n"
-            f"        FROM\n"
-            f'            "public".user u \n'
-            f"            JOIN profiles p ON u.user_id = p.owner\n"
-            f"            JOIN user_group ug ON ug.user_id = u.user_id \n"
-            f'            JOIN "groups" g ON g.group_id = ug.group_id \n'
-            f"            JOIN events e ON e.profile_id_interact = p.profile_id \n"
-            f"        WHERE\n"
-            f"            DATE(e.created_at) = CURRENT_DATE AND e.issue = 'OK'\n"
-            f"        GROUP BY\n"
-            f"            g.group_id \n"
-            f"        HAVING\n"
-            f"            COUNT(DISTINCT p.profile_id) > 0\n"
-            f"    ),\n"
-            f"    Receivers AS (\n"
-            f"        SELECT\n"
-            f"            g.group_id,\n"
-            f"            COUNT(e.profile_id) AS total_clicks_receiver\n"
-            f"        FROM\n"
-            f'            "public".user u \n'
-            f"            JOIN profiles p ON u.user_id = p.owner\n"
-            f"            JOIN user_group ug ON ug.user_id = u.user_id \n"
-            f'            JOIN "groups" g ON g.group_id = ug.group_id \n'
-            f"            JOIN events e ON e.profile_id  = p.profile_id \n"
-            f"        WHERE\n"
-            f"            DATE(e.created_at) = CURRENT_DATE AND e.issue = 'OK'\n"
-            f"        GROUP BY\n"
-            f"            g.group_id \n"
-            f"        HAVING\n"
-            f"            COUNT(DISTINCT p.profile_id) > 0\n"
-            f"    )\n"
-            f"    -- Step 2: Update groups table with the click_count and receiver_count\n"
-            f'    UPDATE "groups" AS g\n'
-            f"    SET click_count = COALESCE(c.total_clicks_giver, 0),\n"
-            f"        receiver_count = COALESCE(r.total_clicks_receiver, 0)\n"
-            f"    FROM Clicks c\n"
-            f"    JOIN Receivers r ON c.group_id = r.group_id\n"
-            f"    WHERE g.group_id = c.group_id;\n"
-            f"    "
-        )
+        sql_query = """
+            WITH Clicks AS (
+                SELECT
+                    g.group_id,
+                    COUNT(e.profile_id_interact) AS total_clicks_giver
+                FROM
+                    "public".user u 
+                    JOIN profiles p ON u.user_id = p.owner
+                    JOIN user_group ug ON ug.user_id = u.user_id 
+                    JOIN "groups" g ON g.group_id = ug.group_id 
+                    JOIN events e ON e.profile_id_interact = p.profile_id 
+                WHERE
+                    DATE(e.created_at) = CURRENT_DATE AND e.issue = 'OK'
+                GROUP BY
+                    g.group_id 
+                HAVING
+                    COUNT(DISTINCT p.profile_id) > 0
+            ),
+            Receivers AS (
+                SELECT
+                    g.group_id,
+                    COUNT(e.profile_id) AS total_clicks_receiver
+                FROM
+                    "public".user u 
+                    JOIN profiles p ON u.user_id = p.owner
+                    JOIN user_group ug ON ug.user_id = u.user_id 
+                    JOIN "groups" g ON g.group_id = ug.group_id 
+                    JOIN events e ON e.profile_id  = p.profile_id 
+                WHERE
+                    DATE(e.created_at) = CURRENT_DATE AND e.issue = 'OK'
+                GROUP BY
+                    g.group_id 
+                HAVING
+                    COUNT(DISTINCT p.profile_id) > 0
+            )
+            -- Step 2: Update groups table with the click_count and receiver_count
+            UPDATE "groups" AS g
+            SET
+                click_count = COALESCE(c.total_clicks_giver, 0),
+                receiver_count = COALESCE(r.total_clicks_receiver, 0)
+            FROM
+                Clicks c
+                LEFT JOIN Receivers r ON c.group_id = r.group_id
+            WHERE
+                g.group_id = c.group_id;
+        
+        """
 
         try:
             # Execute the raw SQL query
             db.session.execute(sql_query)
             db.session.commit()
-            print('Update click count ok')
+            print("Update click count ok")
         except Exception as e:
             # Rollback transaction if an error occurs
             db.session.rollback()
