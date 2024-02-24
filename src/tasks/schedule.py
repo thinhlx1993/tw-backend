e36fb8f2f20a3cc4b4a70cd9f32d3bb584774475
thinh <thinhle.ict@gmail.com>
2024-02-24 09:44:02 +0700

    update set click count

--- src/tasks/schedule.py
+++ src/tasks/schedule.py
@@ -70,8 +70,8 @@ def reset_click(teams_id: str):
         db.session.execute("SET search_path TO public, 'cs_" + teams_id + "'")
         # Update operation to set click_count to zero for all profiles
         try:
-            stmt = update(Profiles).values(click_count=0, comment_count=0, like_count=0)
-            db.session.execute(stmt)
+            sql_query = 'update profiles set click_count=0'
+            db.session.execute(sql_query)
             db.session.commit()
             print("reset_click_count OK")
         except Exception as e:
